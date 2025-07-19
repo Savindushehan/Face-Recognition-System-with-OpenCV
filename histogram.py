394fb7d4b17d8cb06aaa561444e6b73b2019c38b
Savindushehan <Savindushehansamaraweera@gmail.com>
2025-07-19 09:56:19 +0530

    Completed color histrogram

--- histogram.py
+++ histogram.py
@@ -2,20 +2,31 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 img = cv.imread('Photos/cat.jpg')
-cv.imshow('Cats',img)
+cv.imshow('Cats', img)
 
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow('GRay',gray)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
 
 # Grayscale histogram
-gray_hist = cv.calcHist([gray],[0],None,[256],[0,256])
+gray_hist = cv.calcHist([gray], [0], None, [256], [0, 256])
 
 plt.figure()
-plt.title('Grayscale of Histogram')
+plt.title('Grayscale Histogram')
 plt.xlabel('Bins')
-plt.ylabel('# of pixel')
+plt.ylabel('# of Pixels')
 plt.plot(gray_hist)
-plt.xlim([0,256])
+plt.xlim([0, 256])
+
+# Color histograms
+colors = ('b', 'g', 'r')
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], None, [256], [0, 256])
+    plt.plot(hist, color=col)  # Fixed here
+    plt.xlim([0, 256])
+
+plt.title('Color Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of Pixels')
 plt.show()
 
 cv.waitKey(0)
